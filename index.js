86b68d4ac1bd5c0f39dd9d458161ff1f299f1767
azmainarif10 <azmainarif10@gmail.com>
2025-11-08 23:45:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const PORT=3000;
+app.use(express.json())
+app.use(cors())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ae.lom2zra.mongodb.net/?appName=aE"`
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+ async function run(){
+
+
+    try{
+
+           await client.connect()
+           await client.db("billdb").command({ping:1})
+     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+     
+     
+     app.listen(PORT ,()=>{
+        
+        console.log(`Express Server is running on http://localhost:${PORT}`);
+
+     })
+
+    }catch(error){
+       console.log(error)
+    }
+    
+
+
+ }
+run().catch(console.dir);
